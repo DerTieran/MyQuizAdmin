d8425eaae3f2ea4f0a6b70e7568bd0d026b145ec
Marco <Marco@DESKTOP-H1271BC>
2017-01-22 18:36:10 +0100

    Add Static View ( incomplete)

--- MyQuizAdmin/MyQuizAdmin/Views/Page1.xaml.cs
+++ MyQuizAdmin/MyQuizAdmin/Views/Page1.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Windows.UI.Xaml.Controls;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 namespace MyQuizAdmin.Views
 {
@@ -8,9 +10,58 @@ namespace MyQuizAdmin.Views
     /// </summary>
     public sealed partial class Page1 : Page
     {
+        StaticTestData staticTestData;
+        Group group;
+        Question question;
+        bool LayoutUpdateFlag = true;
+
         public Page1()
         {
             this.InitializeComponent();
         }
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            staticTestData = new StaticTestData();
+            staticTestData.id = 0;
+            staticTestData.group.Add(new Group { id = 0, titel = "Angewandte Informatik" });
+
+            staticTestData.group[0].question.Add(new Question { id = 0, text = "Wie war die Vorlesung" });
+            staticTestData.group[0].question[0].listAnswerId.Add(new ListAnswersID { answer = "sehr gut", amount = 4 });
+            staticTestData.group[0].question[0].listAnswerId.Add(new ListAnswersID { answer = "gut", amount = 5 });
+            staticTestData.group[0].question[0].listAnswerId.Add(new ListAnswersID { answer = "schlecht", amount = 2 });
+            staticTestData.group[0].question[0].listAnswerId.Add(new ListAnswersID { answer = "sehr schlecht", amount = 1 });
+            staticTestData.group[0].question.Add(new Question { id = 1, text = "Fandet ihr die Präsentation gut" });
+            staticTestData.group[0].question[1].listAnswerId.Add(new ListAnswersID { answer = "sehr gut", amount = 2 });
+            staticTestData.group[0].question[1].listAnswerId.Add(new ListAnswersID { answer = "gut", amount = 1 });
+            staticTestData.group[0].question[1].listAnswerId.Add(new ListAnswersID { answer = "schlecht", amount = 8 });
+            staticTestData.group[0].question[1].listAnswerId.Add(new ListAnswersID { answer = "sehr schlecht", amount = 11 });
+            g_staticPag.DataContext = staticTestData.group[staticTestData.id];
+            cbx_groups.ItemsSource = staticTestData.group;
+        }
+
+        private void lbx_question_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var AnSelected = lbx_question.SelectedItem as Question;
+            if (AnSelected.listAnswerId != null)
+            {
+                (ColumnChart.Series[0] as ColumnSeries).ItemsSource = AnSelected.listAnswerId;
+            }
+        }
+
+        private void cbx_groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = cbx_groups.SelectedItem as Group;
+            group = item;
+            lbx_question.ItemsSource = group.question;
+        }
+
+        private void txb_search_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.lbx_question.ItemsSource = group.question.Where(w => w.text.ToUpper().Contains(txb_search.Text.ToUpper()) || w.text.ToUpper().Contains(txb_search.Text.ToUpper()));
+            LayoutUpdateFlag = true;
+        }
+
+ 
     }
 }
---
+++ MyQuizAdmin/MyQuizAdmin/Views/StaticTestData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizAdmin.Views
+{
+    public class StaticTestData
+    {
+        public int id { get; set; }
+        public List<Group> group { get; set; }
+
+
+        public StaticTestData()
+        {
+            group = new List<Group>();
+        }
+    }
+
+    public class Group
+    {
+        public int id { get; set; }
+        public string titel { get; set; }
+        public List<Question> question { get; set; }
+        public List<People> people { get; set; }
+
+        public Group()
+        {
+            question = new List<Question>();
+            people = new List<People>();
+        }
+    }
+
+    public class People
+    {
+        public int idPeop { get; set; }
+        public string name { get; set; }
+        public List<QuestionPeople> questionPeople { get; set; }
+
+        public People()
+        {
+            questionPeople = new List<QuestionPeople>();
+        }
+    }
+
+    public class QuestionPeople
+    {
+        public int id { get; set; }
+        public string textPeop { get; set; }
+        public List<ListAnswersID> listAnswerId { get; set; }
+
+        public QuestionPeople()
+        {
+            listAnswerId = new List<ListAnswersID>();
+        }
+    }
+
+
+    public class Question
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public List<ListAnswersID> listAnswerId { get; set; }
+
+        public Question()
+        {
+            listAnswerId = new List<ListAnswersID>();
+        }
+    }
+
+    public class ListAnswersID
+    {
+        public string answer { get; set; }
+        public int amount { get; set; }
+    }
+}
