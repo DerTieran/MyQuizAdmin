509c9daaf64ad626abb78044aa6f79958eb73b2c
Philipp Beck <dertieran@gmail.com>
2017-01-24 11:37:48 +0100

    Basic request class

---
+++ MyQuizAdmin/MyQuizAdmin/Request.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace MyQuizAdmin
+{
+    class Request
+    {
+        private const string BaseAddress = "http://h2653223.stratoserver.net/api";
+
+        private async Task<T> GET<T>(string path)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseAddress);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            T result = default(T);
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<T>(json);
+            }
+            return result;
+        }
+
+        private async Task<Uri> POST<T>(string path, T data)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseAddress);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(path, content);
+            response.EnsureSuccessStatusCode();
+
+            return response.Headers.Location;
+        }
+
+        private async Task<HttpStatusCode> DELETE(string path)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseAddress);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.DeleteAsync(path);
+            return response.StatusCode;
+        }
+    }
+}
