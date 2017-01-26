b2f8586600a6f41ad1bbdf5d97969d4182b8690c
Samer Aldefai <eldefay@gmail.com>
2017-01-26 09:40:06 +0100

    Basic Layout

--- MyQuizAdmin/MyQuizAdmin/MainPage.xaml.cs
+++ MyQuizAdmin/MyQuizAdmin/MainPage.xaml.cs
@@ -28,8 +28,8 @@ namespace MyQuizAdmin
                 new NavMenuItem()
                 {
                     Symbol = Symbol.Contact,
-                    Label = "Page1",
-                    DestinationPage = typeof(Views.Page1)
+                    Label = "GroupView",
+                    DestinationPage = typeof(Views.GroupView)
                 },
                 new NavMenuItem()
                 {
--- MyQuizAdmin/MyQuizAdmin/Views/GroupView.xaml.cs
+++ MyQuizAdmin/MyQuizAdmin/Views/GroupView.xaml.cs
@@ -6,9 +6,9 @@ namespace MyQuizAdmin.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Page1 : Page
+    public sealed partial class GroupView : Page
     {
-        public Page1()
+        public GroupView()
         {
             this.InitializeComponent();
         }
