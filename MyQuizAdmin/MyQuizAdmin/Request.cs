7c3b2daa1823346c4c188075e51cde153bf15a31
Marco Mohr <mohr-marco1@gmx.de>
2017-02-02 11:27:52 +0100

    Statistic view (#14)
    * Add Static View (
    incomplete)
    
    * view responsiv and first rout to group fin
    
    * add request topic
    
    * remove comments and pass selection

--- MyQuizAdmin/MyQuizAdmin/Request.cs
+++ MyQuizAdmin/MyQuizAdmin/Request.cs
@@ -31,7 +31,7 @@ namespace MyQuizAdmin
         private async Task<T> GET<T>(string path)
         {
             HttpClient client = getClient();
-            
+     
             T result = default(T);
             HttpResponseMessage response = await client.GetAsync(path);
             if (response.IsSuccessStatusCode)
@@ -64,10 +64,48 @@ namespace MyQuizAdmin
             HttpResponseMessage response = await client.DeleteAsync(path);
             return response.StatusCode;
         }
-
+      
         public async Task<RegistrationResponse> register(RegistrationData auth)
         {
             RegistrationResponse result = await POST<RegistrationResponse>("/api/devices", auth);
+            return result;
+        }
+
+        public async Task <List<GroupResponse>> GetGroups()
+        {
+            List<GroupResponse> result = await GET<List<GroupResponse>>("/api/groups");
+            return result;
+        }
+
+        public async Task <List<Topic>> getTopicsForGroup(GroupResponse group)
+        {
+            List<Topic> result = await GET<List<Topic>>("/api/groups/" + group.Id + "/topics");          
+            return result;
+        }
+
+        public async Task <List<Result>> getResultForTopicInGroup(Topic topic, GroupResponse group)
+        {
+            //Result result = await GET<Result>("/api/groups/" + group.id + "/topics/" + topic.id + "/results");
+            List<Result.Answer> resultAnswers = new List<Result.Answer>();
+            resultAnswers.Add(new Result.Answer { amount = 50, text="gut" });
+            resultAnswers.Add(new Result.Answer { amount = 10, text = " sehr gut" });
+            resultAnswers.Add(new Result.Answer { amount = 20, text = "schlecht" });
+            List<Result> result = new List<Result>();
+            result.Add(new Result { questionText = "Wie war das Wetter", resultAnswers = resultAnswers });
+            result.Add(new Result { questionText = "Wie war der Schnabeltier", resultAnswers = resultAnswers });
+            return result;
+        }
+
+        public async Task<List<Result>> getResultForGroup(GroupResponse group)
+        {
+            //Result result = await GET<Result>("/api/groups/" + group.id + "/results");
+            List<Result.Answer> resultAnswers = new List<Result.Answer>();
+            resultAnswers.Add(new Result.Answer { amount = 50, text = "gut" });
+            resultAnswers.Add(new Result.Answer { amount = 10, text = " sehr gut" });
+            resultAnswers.Add(new Result.Answer { amount = 20, text = "schlecht" });
+            List<Result> result = new List<Result>();
+            result.Add(new Result { questionText = "Wie war das Wetter", resultAnswers = resultAnswers });
+            result.Add(new Result { questionText = "Wie war der Schnabeltier", resultAnswers = resultAnswers });
             return result;
         }
     }
--- MyQuizAdmin/MyQuizAdmin/Views/Page1.xaml.cs
+++ MyQuizAdmin/MyQuizAdmin/Views/Page1.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+using MyQuizAdmin.Models;
+using System.Collections.Generic;
+using MyQuizAdmin;
 
 namespace MyQuizAdmin.Views
 {
@@ -8,9 +14,45 @@ namespace MyQuizAdmin.Views
     /// </summary>
     public sealed partial class Page1 : Page
     {
+        
+        bool LayoutUpdateFlag = true;
+        Request request = new Request();
+
         public Page1()
         {
             this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            List<GroupResponse> groupResponList = await request.GetGroups();             
+            cbx_groups.ItemsSource = groupResponList;          
+        }
+
+        private async void cbx_groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var SelectedGroup = cbx_groups.SelectedItem as GroupResponse;
+            List<Topic> topicResponList = await request.getTopicsForGroup(SelectedGroup);
+            lbx_people.ItemsSource = topicResponList;
+
+        }
+
+        private async void txb_searchpeople_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var topicList = await request.getTopicsForGroup(cbx_groups.SelectedItem as GroupResponse);
+            if (lbx_people.ItemsSource != null)
+            {
+                this.lbx_people.ItemsSource = topicList.Where(w => w.String.ToUpper().Contains(txb_searchpeople.Text.ToUpper()) || w.String.ToUpper().Contains(txb_searchpeople.Text.ToUpper()));
+                LayoutUpdateFlag = true;
+            }
+        }
+
+        private async void lbx_people_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var SelectedGroup = cbx_groups.SelectedItem as GroupResponse;
+            var SelectedTopic = lbx_people.SelectedItem as Topic;
+            lv_static.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            lv_static.ItemsSource = await request.getResultForTopicInGroup(SelectedTopic, SelectedGroup);
         }
     }
 }
