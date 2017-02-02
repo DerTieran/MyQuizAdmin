0c7ff913f90627de07545e3925f9b44a9b0ca02a
Philipp Beck <dertieran@gmail.com>
2017-02-02 13:05:09 +0100

    Add new models

---
+++ MyQuizAdmin/MyQuizAdmin/Models/QuestionBlock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyQuizAdmin.Models
+{
+    public class QuestionBlock
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = "Fragen Block " + new Random().Next();
+    }
+}
