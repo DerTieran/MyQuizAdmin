ef9acc03988b3a5692c4a4be2f4a0fd47cf9b6c3
Daniel <Daniel@DESKTOP-6C2SIF7>
2017-02-04 14:14:42 +0100

    Merge erfolgreich

---
+++ MyQuizAdmin/MyQuizAdmin/Models/AnswerOption.cs
@@ -0,0 +1,9 @@
+﻿namespace MyQuizAdmin.Models
+{
+    public class AnswerOption
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public string isCorrect { get; set; }
+    }
+}
--- MyQuizAdmin/MyQuizAdmin/Models/Request.cs
+++ MyQuizAdmin/MyQuizAdmin/Models/Request.cs
@@ -92,18 +92,4 @@ namespace MyQuizAdmin.Models
         }
 
     }
-
-    public class RegistrationData
-    {
-        public string password { get; set; }
-        //public string token { get; set; }
-        //public string deviceID { get; set; }
-    }
-
-    public class RegistrationResponse
-    {
-        public long Id { get; set; }
-        public long? IsAdmin { get; set; }
-        //public string PushUpToken { get; set; }
-    }
 }
