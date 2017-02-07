3b262ee3f0de9f1af855ee7add816b94ffe168f0
Wummer94 <mohr-marco1@gmx.de>
2017-02-07 14:35:45 +0100

    renamed page1 to statisticview

--- MyQuizAdmin/MyQuizAdmin/Views/StatisticView.xaml.cs
+++ MyQuizAdmin/MyQuizAdmin/Views/StatisticView.xaml.cs
@@ -12,12 +12,12 @@ namespace MyQuizAdmin.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Page1 : Page
+    public sealed partial class StatisticView : Page
     {       
         bool LayoutUpdateFlag = true;
         Request request = new Request();
 
-        public Page1()
+        public StatisticView()
         {
             this.InitializeComponent();
         }
